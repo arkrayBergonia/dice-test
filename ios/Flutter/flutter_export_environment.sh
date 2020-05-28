cf96a4f967b25fb2f52bdd319f8f73558b075ef3
arkrayBergonia <fbergonia.arkray@gmail.com>
2020-05-28 15:40:28 +0800

    initial commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/francisyuweh/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/francisyuweh/Downloads/SwiftUI/FlutterP/dicee-flutter-master"
+export "FLUTTER_TARGET=/Users/francisyuweh/Downloads/SwiftUI/FlutterP/dicee-flutter-master/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/francisyuweh/Developer/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
